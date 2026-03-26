ea085d9dcd6c0e27f4747b329b55733596341225
tfshorifulislam <tfshorifulislam@gmail.com>
2026-03-27 05:11:14 +0600

    selected and available players dynamicly add and all issue fixed

--- src/Components/Card/SelectedPlayers.jsx
+++ src/Components/Card/SelectedPlayers.jsx
@@ -14,21 +14,26 @@ const SelectedPlayers = ({ selectedPlayers, setSelectedPlayers, setCoin, coin })
         <div>
             <div className='mt-8 space-y-6'>
                 {
-                    selectedPlayers.map((player, i) => {
-                        return (
-                            <div key={i}
-                                className='flex justify-between items-center p-3 md:p-6 border border-[#13131310] shadow-xl rounded-2xl'>
-                                <div className='flex gap-6 flex-col md:flex-row'>
-                                    <img className='w-25 h-18.75 rounded-lg' src={player.playerImg} alt={player.PlayerName} />
-                                    <div>
-                                        <h2 className='font-semibold text-[#131313] text-2xl'>{player.PlayerName}</h2>
-                                        <p>{player.playerType}</p>
+                    selectedPlayers.length === 0 ?
+                        <div className='flex flex-col justify-center items-center h-56'>
+                            <h1 className='font-bold text-2xl '>No Players Selected Yet</h1>
+                            <p>go to Available tab to select Players</p>
+                        </div>
+                        : selectedPlayers.map((player, i) => {
+                            return (
+                                <div key={i}
+                                    className='flex justify-between items-center p-3 md:p-6 border border-[#13131310] shadow-xl rounded-2xl'>
+                                    <div className='flex gap-6 flex-col md:flex-row'>
+                                        <img className='w-25 h-18.75 rounded-lg' src={player.playerImg} alt={player.PlayerName} />
+                                        <div>
+                                            <h2 className='font-semibold text-[#131313] text-2xl'>{player.PlayerName}</h2>
+                                            <p>{player.playerType}</p>
+                                        </div>
                                     </div>
+                                    <MdDelete className='btn text-[#F14749]' onClick={() => handleDeleteClicked(player)} />
                                 </div>
-                                <MdDelete className='btn text-[#F14749]' onClick={() => handleDeleteClicked(player)} />
-                            </div>
-                        )
-                    })
+                            )
+                        })
                 }
             </div>
         </div>
