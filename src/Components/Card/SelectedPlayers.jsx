3b649f28c4457907bf1a72e5f128915f9e65aa77
tfshorifulislam <tfshorifulislam@gmail.com>
2026-03-27 04:04:27 +0600

    all players filter and delete system add with coin back

--- src/Components/Card/SelectedPlayers.jsx
+++ src/Components/Card/SelectedPlayers.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { MdDelete } from "react-icons/md";
 
-const SelectedPlayers = ({selectedPlayers}) => {
+const SelectedPlayers = ({ selectedPlayers, setSelectedPlayers, setCoin, coin }) => {
     console.log(selectedPlayers)
+    const handleDeleteClicked = (player) => {
+        const filteredPlayers = selectedPlayers.filter(selectDeletePlayers => selectDeletePlayers.PlayerName !== player.PlayerName)
+        setSelectedPlayers(filteredPlayers)
+        setCoin(coin + player.price)
+    }
     return (
         <div>
-            <h1>Selected Players</h1>
+            <div className='mt-8 space-y-6'>
+                {
+                    selectedPlayers.map((player, i) => {
+                        return (
+                            <div key={i}
+                                className='flex justify-between items-center p-3 md:p-6 border border-[#13131310] shadow-xl rounded-2xl'>
+                                <div className='flex gap-6 flex-col md:flex-row'>
+                                    <img className='w-25 h-18.75 rounded-lg' src={player.playerImg} alt={player.PlayerName} />
+                                    <div>
+                                        <h2 className='font-semibold text-[#131313] text-2xl'>{player.PlayerName}</h2>
+                                        <p>{player.playerType}</p>
+                                    </div>
+                                </div>
+                                <MdDelete className='btn text-[#F14749]' onClick={()=>handleDeleteClicked(player)} />
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     );
 };
--- src/Components/Players/Players.jsx
+++ src/Components/Players/Players.jsx
@@ -1,30 +1,33 @@
-import React, { useState } from 'react';
+import React, { use, useState } from 'react';
 import AvailablePlayers from '../Card/AvailablePlayers';
 import SelectedPlayers from '../Card/SelectedPlayers';
 
 
 const Players = ({ playersPromise, setCoin, coin }) => {
-
+    const players = use(playersPromise)
     const [selectedPlayers, setSelectedPlayers] = useState([])
     const [selectedType, setSelectedType] = useState('Available')
     // console.log(selectedType)
     return (
         <div>
             <div className='flex justify-between mt-20'>
-                <h2 className='font-bold text-3xl text-[#131313]'>{selectedType === 'Available' ? 'Available Players' : 'Selected Players'}</h2>
+                {selectedType === 'Available' ?
+                    <h2 className='font-bold text-3xl text-[#131313]'>Available Players</h2>
+                    : <h2 className='font-bold text-3xl text-[#131313]'> Selected Players ({selectedPlayers.length} / {players.length})</h2>
+                }
                 <div>
                     <button
                         onClick={() => setSelectedType('Available')}
                         className={`btn ${selectedType === 'Available' ? 'bg-[#04663a]' : 'btn'}  font-bold rounded-r-none border-none rounded-l-xl text-black`}>Available</button>
                     <button
                         onClick={() => setSelectedType('Selected')}
-                        className={`btn ${selectedType === 'Selected' ? 'bg-[#04663a]' : 'btn'}  font-bold rounded-l-none border-none rounded-r-xl text-black`}>Selected (0)</button>
+                        className={`btn ${selectedType === 'Selected' ? 'bg-[#04663a]' : 'btn'}  font-bold rounded-l-none border-none rounded-r-xl text-black`}>Selected ({selectedPlayers.length})</button>
                 </div>
             </div>
             {
                 selectedType === 'Available' ?
                     <AvailablePlayers playersPromise={playersPromise} coin={coin} setCoin={setCoin} selectedPlayers={selectedPlayers} setSelectedPlayers={setSelectedPlayers} />
-                    : <SelectedPlayers selectedPlayers={selectedPlayers} />
+                    : <SelectedPlayers selectedPlayers={selectedPlayers} setSelectedPlayers={setSelectedPlayers} coin={coin} setCoin={setCoin}/>
             }
         </div>
     );
